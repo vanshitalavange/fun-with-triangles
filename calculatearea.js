cef20692f86f30ccdfe66f66643f56fdca6fa84d
vanshitalavange <vanshita.a.lavange@gmail.com>
2021-09-28 19:30:28 +0530

    area of triangle done

---
+++ calculatearea.js
@@ -0,0 +1,17 @@
+const sides = document.querySelectorAll('.side-input');
+const areaBtn = document.querySelector('#area-btn');
+const outputEl = document.querySelector('#output');
+
+function getProduct(base,height){
+
+    let product = 0.5 * base * height;
+    return product;
+
+}
+function calculateAreaOfTriangle(){
+
+   let area = getProduct(Number(sides[0].value),Number(sides[1].value));
+   outputEl.textContent="The area is "+area;
+}
+
+areaBtn.addEventListener("click",calculateAreaOfTriangle);
