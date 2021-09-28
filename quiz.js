08f2028d3818018f908b7eb08648021ace9f2697
vanshitalavange <vanshita.a.lavange@gmail.com>
2021-09-28 17:19:57 +0530

    quiz questions added

---
+++ quiz.js
@@ -0,0 +1,21 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitAnswerBtn = document.querySelector("#submit-answer-btn");
+const outputEl = document.querySelector("#output");
+
+const correctAnswers = [3, 30];
+function calculateScore() {
+  let index= 0;
+  let score=0;
+  const formResults = new FormData(quizForm);
+  for(let value of formResults.values()){
+        if(value == correctAnswers[index]){
+            score = score + 1;
+        }
+    
+    index=index+1;
+  }
+
+  outputEl.textContent = "Your score is " +score;
+}
+
+submitAnswerBtn.addEventListener("click", calculateScore);
