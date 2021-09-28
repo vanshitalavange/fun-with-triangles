2926c901a46e91e4b1149634c3e781fe2481d6ad
vanshitalavange <vanshita.a.lavange@gmail.com>
2021-09-29 03:10:42 +0530

    refactor : code format

--- istriangle.js
+++ istriangle.js
@@ -7,23 +7,24 @@ function calculateSumOfAngles(angle1, angle2, angle3) {
 }
 
 function isTriangle() {
-    if(inputs[0].value==="" ||inputs[1].value==="" ||inputs[2].value==="")
-    {
-        outputEl.textContent="Please enter all 3 angles 🙄";
+  if (
+    inputs[0].value === "" ||
+    inputs[1].value === "" ||
+    inputs[2].value === ""
+  ) {
+    outputEl.textContent = "Please enter all 3 angles 🙄";
+  } else {
+    const sumOfAngles = calculateSumOfAngles(
+      Number(inputs[0].value),
+      Number(inputs[1].value),
+      Number(inputs[2].value)
+    );
+    if (sumOfAngles === 180) {
+      outputEl.textContent = "The angles form a triangle 😀";
+    } else {
+      outputEl.textContent = "The angles don't form a triangle 🥴";
     }
-    else{
-  const sumOfAngles = calculateSumOfAngles(
-    Number(inputs[0].value),
-    Number(inputs[1].value),
-    Number(inputs[2].value)
-  );
-  if(sumOfAngles === 180){
-     outputEl.textContent ="The angles form a triangle 😀";
   }
-  else{
-      outputEl.textContent ="The angles don't form a triangle 🥴";
-  }
-}
 }
 
 isTriangleBtn.addEventListener("click", isTriangle);
